055df3459b9ed422ab2cb06ad546a21011ecc14b
Orlando Gelves <orlandoantoni@hotmail.com>
2020-08-17 19:31:43 -0500

    Developing finish

---
+++ src/main/java/edu/escuelaing/arep/SparkAPI.java
@@ -0,0 +1,33 @@
+package edu.escuelaing.arep;
+import edu.escuelaing.arep.components.*;
+import org.json.JSONObject;
+
+import static spark.Spark.*;
+
+
+public class SparkAPI {
+    public static void main(String[] args) {
+        getPort();
+        get("/calculator",(req,res)-> {
+            String values = req.queryParams("numbers");
+            String[] valuesInStringArray = values.split(",");
+            LinkedList linkedList = new LinkedList();
+            for(String v:valuesInStringArray){
+                linkedList.addNode(Float.parseFloat(v));
+            }
+            double mean = Calculator.mean(linkedList);
+            double deviation = Calculator.deviation(linkedList,mean);
+            JSONObject jsonValue = new JSONObject();
+            jsonValue.put("numbers",String.join(" ",valuesInStringArray));
+            jsonValue.put("mean",mean);
+            jsonValue.put("deviation",deviation);
+            return jsonValue;
+        });
+    }
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+}
